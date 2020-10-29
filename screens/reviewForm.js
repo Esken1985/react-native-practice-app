b292f44e47f60f09b2f584b57712cb22e41b3f55
Eskender <esken999@gmail.com>
2020-10-29 08:52:17 +0300

    displaying form errors and creating a custom button

--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Button, TextInput, View } from "react-native";
+import { TextInput, View, Text } from "react-native";
 import { globalStyles } from "../styles/global.js";
 import { Formik } from "formik";
 import * as yup from "yup";
+import FlatButton from "../shared/button.js";
 
 const reviewSchema = yup.object({
   title: yup.string().required().min(4),
@@ -33,26 +34,28 @@ export default function ReviewForm({ addReview }) {
               placeholder="Review title"
               onChangeText={props.handleChange("title")}
               value={props.values.title}
+              onBlur={props.handleBlur("title")}
             />
+            <Text style={globalStyles.errorText}>{props.touched.title && props.errors.title}</Text>
             <TextInput
-              multiline
+              multiline minHeight={100}
               style={globalStyles.input}
               placeholder="Review body"
               onChangeText={props.handleChange("body")}
               value={props.values.body}
+              onBlur={props.handleBlur("body")}
             />
+            <Text style={globalStyles.errorText}>{props.touched.body && props.errors.body}</Text>
             <TextInput
               style={globalStyles.input}
               placeholder="Rating (1-5)"
               onChangeText={props.handleChange("rating")}
               value={props.values.rating}
+              onBlur={props.handleBlur("rating")}
               keyboardType="numeric"
             />
-            <Button
-              title="Submit"
-              color="maroon"
-              onPress={props.handleSubmit}
-            />
+            <Text style={globalStyles.errorText}>{props.touched.rating && props.errors.rating}</Text>
+            <FlatButton text="submit" onPress={props.handleSubmit}/>
           </View>
         )}
       </Formik>
